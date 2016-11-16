4867635498b4576786a15ec433a407a5726ebc7f
Patrick Segura <psegur@Patricks-MacBook-Pro-2.local>
2016-11-15 18:29:29 -0600

    5

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,41 +14,23 @@ resetChoices();
 
 var player1Storage = {
 	player: false,
-	name: 'No Name',
 	playerWins: 0,
 	playerLosses: 0,
 	playerChoice: '',
-	oneOrTwo: 1,
-	answer: '',
-	answerOld: ''
-}
+	choiceOld: ''
+};
 
 var player2Storage = {
 	player: false,
-	name: 'No Name',
 	playerWins: 0,
 	playerLosses: 0,
 	playerChoice: '',
-	oneOrTwo: 1,
-	answer: '',
-	answerOld: ''
+	choiceOld: ''
 };
 
-var player1 = false;
-var player2 = false;
 var name = 'No Name';
-var player1Wins = 0;
-var player2Wins = 0;
-var player1Losses = 0;
-var player2Losses = 0;
-var player1Choice = '';
-var player2Choice = '';
 var oneOrTwo = 1;
-var answer1 = '';
-var answer2 = '';
-var answerOld1 = '';
-var answerOld2 = '';
-
+var chatCounter = 0000;
 
 database.ref().child('initialize').set({
 	active: true 
@@ -62,46 +44,87 @@ function printInitial() {
 	}
 }
 
+function printChat() {
+	var chat = database.ref().child('chat');
+	var lastMessage = '';
+	$(document).on('submit', 'form', function(event) {
+		event.preventDefault(); 
+		var message = name + ': ' + ($('#chat-input').val().trim());
+		database.ref().child('chatCount').set({
+			chatCounts: chatCounter
+		});
+		chat.child('chat' + chatCounter).set({
+			chat: message
+		});
+		var lastMessage = '';
+		$('#chat-input').val('');
+	});
+	chat.orderByChild("dateAdded").on('child_added', function(snapshot) {
+		lastMessage = snapshot.val().chat;
+		console.log(lastMessage);
+		var chatBox = $('#chat-output');
+		chatBox.append(lastMessage + '\n');
+		$(chatBox).scrollTop($(chatBox)[0].scrollHeight);
+	});
+}
+
 database.ref().on('value', function(snapshot) {
-	answer1 = snapshot.val().player_1.choice;
-	answer2 = snapshot.val().player_2.choice;
-	if (player1New !=== player1Old && player2New !=== player2Old) {
-		if (answer1 === "rock" && answer2 === "paper") {
+
+	var chatNode = snapshot.child('chatCount');
+	chatCounter = chatNode.child('chatCounts').val();
+	chatCounter++;
+	var p1Node = snapshot.child('player_1');
+	var answer1 = p1Node.child('choice').val();
+	var answer1Old = p1Node.child('oldChoice').val();
+	var p2Node = snapshot.child('player_2');
+	var answer2 = p2Node.child('choice').val();
+	var answer2Old = p2Node.child('oldChoice').val();
+
+	if (answer1 !== answer1Old && answer2 !== answer2Old) {
+		if (answer1 === "Rock" && answer2 === "Paper") {
 			alert("Player 2 wins!");
-			p2score = p2score++;
+			uponWin(answer1, answer2);
+			// p2score = p2score++;
 		}
-		else if (answer1 === "rock" && answer2 === "scissors") {
+		else if (answer1 === "Rock" && answer2 === "Scissors") {
 			alert("Player 1 wins!");
-			p2score = p1score++;
+			uponWin(answer1, answer2);
+			// p2score = p1score++;
 		}
-		else if (answer1 === "rock" && answer2 === "rock") {
+		else if (answer1 === "Rock" && answer2 === "Rock") {
 			alert("Tie");
+			uponWin(answer1, answer2);
 		}
-		else if (answer1 === "paper" && answer2 === "rock") {
+		else if (answer1 === "Paper" && answer2 === "Rock") {
 			alert("Player 1 wins!");
-			p2score = p1score++;
+			uponWin(answer1, answer2);
+			// p2score = p1score++;
 		}
-		else if (answer1 === "paper" && answer2 === "paper") {
+		else if (answer1 === "Paper" && answer2 === "Paper") {
 			alert("Tie");
+			uponWin(answer1, answer2);
 		}
-		else if (answer1 === "paper" && answer2 === "scissors") {
+		else if (answer1 === "Paper" && answer2 === "Scissors") {
 			alert("Player 2 wins!");
-			p2score = p1score++;
+			uponWin(answer1, answer2);
+			// p2score = p1score++;
 		}
-		else if (answer1 === "scissors" && answer2 === "paper") {
+		else if (answer1 === "Scissors" && answer2 === "Paper") {
 			alert("Player 1 wins!");
-			p2score = p1score++;
+			uponWin(answer1, answer2);
+			// p2score = p1score++;
 		}
-		else if (answer1 === "scissors" && answer2 === "scissors") {
+		else if (answer1 === "Scissors" && answer2 === "Scissors") {
 			alert("Tie");
+			uponWin(answer1, answer2);
 		}
-		else if (answer1 === "scissors" && answer2 === "rock") {
+		else if (answer1 === "Scissors" && answer2 === "Rock") {
 			alert("Player 2 wins!");
-			p2score = p1score++;
+			uponWin(answer1, answer2);
+			// p2score = p1score++;
 		}
 	}
-	answerOld1 = answer1;
-	answerOld2 = answer2;
+
 	if (snapshot.child("player_1").exists() && snapshot.child("player_2").exists()) {
 		updatePlayer1(snapshot);
 		updatePlayer2(snapshot);
@@ -109,18 +132,26 @@ database.ref().on('value', function(snapshot) {
 	if (snapshot.child("player_1").exists()) {
 		updatePlayer1(snapshot);
 	}
+
 });
+
+function uponWin(answer1, answer2) {
+	database.ref().child('player_1').update({
+		oldChoice: answer1,
+	});
+	database.ref().child('player_2').update({
+		oldChoice: answer2,
+	});
+}
 
 function updatePlayer1(snapshot) {
 	$("#p1-name").html(snapshot.val().player_1.name);
-	// $("#p1-choice").html(snapshot.val().player_1.choice);
 	$("#p1-wins").html(snapshot.val().player_1.player1Wins);
 	$("#p1-losses").html(snapshot.val().player_1.player1Losses);
 }
 
 function updatePlayer2(snapshot) {
 	$("#p2-name").html(snapshot.val().player_2.name);
-	// $("#p2-choice").html(snapshot.val().player_2.choice);
 	$("#p2-wins").html(snapshot.val().player_2.player2Wins);
 	$("#p2-losses").html(snapshot.val().player_2.player2Losses);
 }
@@ -130,13 +161,13 @@ function choose() {
 		var chosenID = $(this).parent().attr('id');
 		var choice = $(this).html();
 		if (chosenID === 'p1-choice') {
-			player1Choice = choice;
+			player1Storage.playerChoice = choice;
 			database.ref().child('player_1').update({
 				choice: choice
 			});
 			$('#p1-choice').css('fontSize', '36px').html(choice);
 		} else if (chosenID === 'p2-choice') {
-			player2Choice = choice;
+			player2Storage.playerChoice = choice;
 			database.ref().child('player_2').update({
 				choice: choice
 			});
@@ -162,65 +193,49 @@ function storeName() {
 				$("#p1-name").html(snapshot.val().player_1.name);
 				$("#p2-name").html(snapshot.val().player_2.name);
 			} else if (snapshot.child("player_1").exists()) {
-				player2 = true;
+				player2Storage.player = true;
 				oneOrTwo = 2;
 				$('#name-form').html('Hi ' + name + '! You are player ' + oneOrTwo);
 				database.ref().child('player_2').set({
 					name: name,
-					choice: player2Choice,
-					wins: player2Wins,
-					losses:player2Losses
+					choice: player2Storage.playerChoice,
+					oldChoice: player2Storage.choiceOld,
+					wins: player2Storage.playerWins,
+					losses:player2Storage.playerLosses
 				});
 				$("#p1-name").html(snapshot.val().player_1.name);
 				$('#p2-name').html(name);
 				$('#player1').css('pointerEvents', 'none');
 				$('#name-input').val('');
 			} else {
-				player1 = true;
+				player1Storage.player = true;
 				oneOrTwo = 1;
 				$('#name-form').html('Hi ' + name + '! You are player ' + oneOrTwo);
 				database.ref().child('player_1').set({
 					name: name,
-					choice: player1Choice,
-					wins: player1Wins,
-					losses: player1Losses
+					choice: player1Storage.playerChoice,
+					oldChoice: player1Storage.choiceOld,
+					wins: player1Storage.playerWins,
+					losses:player1Storage.playerLosses
 				});
 				$("#p1-name").html(name);
 				$('#player2').css('pointerEvents', 'none');
 				$('#name-input').val('');
-			} 
+			}
 		});
 		return false;
 	});
-}
-
-function printChat() {
-	$(document).on('submit', 'form', function(event) {
-		event.preventDefault(); 
-		var message = ($('#chat-input').val().trim()) + '\n';
-		$('#chat-output').append(name + ': ' + message);
-		$('#chat-input').val('');
-	})
 }
 
 if (name === 'No Name') {
 	$('#player2, #player1').css('pointerEvents', 'none');
 }
 
-
-
-
 printChat();
 printInitial();
 storeName();
 choose();
+
 });
 
-
-// use the key/timestamps of the node in firebase as a way to identify
-
-// take players name
-// assign player one or two
-// make all consequential action be a part of the player's firebase object
-// firebase should just have two play objects, upon quit or loss, one players object is removed
 
